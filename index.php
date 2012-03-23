202944c29034b2b388d4e4220ad7f203c00bb86d
dennis <dennis@dennis-Rev-1-0.(none)>
2012-03-23 16:11:06 +0100

    updae index

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ if (!session::checkAccessControl('locales_allow')){
     return;
 }
 
+if (isset($_POST)) html::specialEncode ($_POST);
+
 locales::displaySetTimezone();
 locales::displaySetLocaleUTF8();
 locales::displaySetLanguage();
