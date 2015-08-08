2e918b9b06dfed1ecdbf4a984b352434e7f93d6e
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-08 15:19:26 +0200

    lang

---
+++ lang/da/language.php
@@ -0,0 +1,33 @@
+<?php
+
+$LANG = array();
+
+$LANG['Load language all into DB'] = 'Load sprog alle i DB';
+$LANG['submit'] = 'indsende';
+$LANG['Search and replace in all strings'] = 'Søg og erstat i alle strengene';
+$LANG['Search string'] = 'Søg snor';
+$LANG['Replace string'] = 'Udskift snor';
+$LANG['Search and replace'] = 'Søg og erstat';
+$LANG['Key'] = 'Nøgle';
+$LANG['No translation in'] = 'Ingen oversættelse i';
+$LANG['Translation in'] = 'Oversættelse i';
+$LANG['Update'] = 'Opdatering';
+$LANG['Edit'] = 'Edit';
+$LANG['Timezone has been updated'] = 'Tidszone er blevet opdateret';
+$LANG['Timezone is not valid'] = 'Tidszone er ikke gyldig';
+$LANG['Reload language_all files from <span class="notranslate">{template}</span> into DB'] = 'Reload language_all filer fra <span class="notranslate">{template}</span> ind DB';
+$LANG['Languages has been reloaded'] = 'Sprog er blevet genindlæses';
+$LANG['DB translation has been updated'] = 'DB oversættelse er blevet opdateret';
+$LANG['No language has been loaded. Presss button to load'] = 'Ingen sprog er blevet indlæst. Presss knappen for at indlæse';
+$LANG['Set timezone for your system'] = 'Indstil tidszone for dit system';
+$LANG['Submit'] = 'Indsend';
+$LANG['Locale has been updated'] = 'Locale er blevet opdateret';
+$LANG['Locale is not valid'] = 'Locale er ikke gyldig';
+$LANG['Set locale. Set e.g. dates and money symbols to your language specifics'] = 'Set locale. Sæt fx datoer og penge symboler til dit sprog detaljerne';
+$LANG['Language is not valid'] = 'Sprog er ikke gyldig';
+$LANG['Set language of interface and HTML document'] = 'Sæt sprog interface og HTML-dokument';
+$LANG['Language and time'] = 'Sprog og tid';
+$LANG['Current date and time according to setup'] = 'Aktuelle dato og tid i henhold til opsætning';
+$LANG['Language'] = 'Sprog';
+$LANG['Base settings'] = 'Grundindstillinger';
+$LANG['Edit translation'] = 'Edit oversættelse';
