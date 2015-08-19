90cc795b460f6dd1dfb1a4f9b2afb06f756e2e56
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-19 17:10:39 +0200

    da

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,23 +2,8 @@
 
 $LANG = array();
 
-$LANG['Load language all into DB'] = 'Load sprog alle i DB';
-$LANG['submit'] = 'indsende';
-$LANG['Search and replace in all strings'] = 'Søg og erstat i alle strengene';
-$LANG['Search string'] = 'Søg snor';
-$LANG['Replace string'] = 'Udskift snor';
-$LANG['Search and replace'] = 'Søg og erstat';
-$LANG['Key'] = 'Nøgle';
-$LANG['No translation in'] = 'Ingen oversættelse i';
-$LANG['Translation in'] = 'Oversættelse i';
-$LANG['Update'] = 'Opdatering';
-$LANG['Edit'] = 'Edit';
 $LANG['Timezone has been updated'] = 'Tidszone er blevet opdateret';
 $LANG['Timezone is not valid'] = 'Tidszone er ikke gyldig';
-$LANG['Reload language_all files from <span class="notranslate">{template}</span> into DB'] = 'Reload language_all filer fra <span class="notranslate">{template}</span> ind DB';
-$LANG['Languages has been reloaded'] = 'Sprog er blevet genindlæses';
-$LANG['DB translation has been updated'] = 'DB oversættelse er blevet opdateret';
-$LANG['No language has been loaded. Presss button to load'] = 'Ingen sprog er blevet indlæst. Presss knappen for at indlæse';
 $LANG['Set timezone for your system'] = 'Indstil tidszone for dit system';
 $LANG['Submit'] = 'Indsend';
 $LANG['Locale has been updated'] = 'Locale er blevet opdateret';
@@ -28,6 +13,5 @@ $LANG['Language is not valid'] = 'Sprog er ikke gyldig';
 $LANG['Set language of interface and HTML document'] = 'Sæt sprog interface og HTML-dokument';
 $LANG['Language and time'] = 'Sprog og tid';
 $LANG['Current date and time according to setup'] = 'Aktuelle dato og tid i henhold til opsætning';
-$LANG['Language'] = 'Sprog';
 $LANG['Base settings'] = 'Grundindstillinger';
 $LANG['Edit translation'] = 'Edit oversættelse';
