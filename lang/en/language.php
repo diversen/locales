7bb3dce789e9d69001678ed9e89d2ab6a14f0256
Dennis Iversen <dennis.iversen@gmail.com>
2015-11-24 12:26:33 +0100

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -2,6 +2,16 @@
 
 $LANG = array();
 
+// Translation of file modules/locales/install.inc
+
+$LANG['Language and time'] = 'Language and time';
+// Translation of file modules/locales/views.php
+
+$LANG['Current date and time according to setup'] = 'Current date and time according to setup';
+// Translation of file modules/locales/menu.inc
+
+$LANG['Base settings'] = 'Base settings';
+$LANG['Edit translation'] = 'Edit translation';
 // Translation of file modules/locales/module.php
 
 $LANG['Timezone has been updated'] = 'Timezone has been updated';
@@ -13,13 +23,3 @@ $LANG['Locale is not valid'] = 'Locale is not valid';
 $LANG['Set locale. Set e.g. dates and money symbols to your language specifics'] = 'Set locale. Set e.g. dates and money symbols to your language specifics';
 $LANG['Language is not valid'] = 'Language is not valid';
 $LANG['Set language of interface and HTML document'] = 'Set language of interface and HTML document';
-// Translation of file modules/locales/install.inc
-
-$LANG['Language and time'] = 'Language and time';
-// Translation of file modules/locales/views.php
-
-$LANG['Current date and time according to setup'] = 'Current date and time according to setup';
-// Translation of file modules/locales/menu.inc
-
-$LANG['Base settings'] = 'Base settings';
-$LANG['Edit translation'] = 'Edit translation';
