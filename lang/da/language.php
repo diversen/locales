d669149b53224ddc8382d22bdd781f4cee234eac
Dennis Iversen <dennis.iversen@gmail.com>
2016-02-17 16:51:46 +0100

    lang

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,6 +2,10 @@
 
 $LANG = array();
 
+$LANG['Language and time'] = 'Sprog og tid';
+$LANG['Current date and time according to setup'] = 'Aktuelle dato og tid i henhold til opsætning';
+$LANG['Base settings'] = 'Grundindstillinger';
+$LANG['Edit translation'] = 'Edit oversættelse';
 $LANG['Timezone has been updated'] = 'Tidszone er blevet opdateret';
 $LANG['Timezone is not valid'] = 'Tidszone er ikke gyldig';
 $LANG['Set timezone for your system'] = 'Indstil tidszone for dit system';
@@ -11,7 +15,3 @@ $LANG['Locale is not valid'] = 'Locale er ikke gyldig';
 $LANG['Set locale. Set e.g. dates and money symbols to your language specifics'] = 'Set locale. Sæt fx datoer og penge symboler til dit sprog detaljerne';
 $LANG['Language is not valid'] = 'Sprog er ikke gyldig';
 $LANG['Set language of interface and HTML document'] = 'Sæt sprog interface og HTML-dokument';
-$LANG['Language and time'] = 'Sprog og tid';
-$LANG['Current date and time according to setup'] = 'Aktuelle dato og tid i henhold til opsætning';
-$LANG['Base settings'] = 'Grundindstillinger';
-$LANG['Edit translation'] = 'Edit oversættelse';
